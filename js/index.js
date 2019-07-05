93d5fd6a9ce70f1c81c62cb75467140121caf28f
Almas Zaurbekov <almasgaara@mail.ru>
2019-07-05 13:17:02 +0600

    add section AboutMe

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
 $(document).ready(function(){
   $(".menu-burger").click(function(){
-  	if($(this).attr('data-toggle') == 0){
-  		$(this).find('i').removeClass('fa-navicon');
-  		$(this).find('i').addClass('fa-arrow-left');
-  		$(this).find('i').css('color', '#E1E2E2');
-		$('.menu').animate({
-	    	width:'20%'
-	    }), $(this).animate({
-	    	left: '15%',
-	    	top: '1%'
-	    }), $('.menu-items').fadeToggle();
-	    $(this).attr('data-toggle', '1');
-	}
-	else{
-		$(this).find('i').addClass('fa-navicon');
-  		$(this).find('i').removeClass('fa-arrow-left');
-  		$(this).find('i').css('color', '#1D2228');
-  		$('.menu-items').fadeToggle(), $('.menu').animate({
-	    	width:'0%'
-	    }), $(this).animate({
-	    	left: '0%',
-	    	top: '0%'
-	    });
-	    $(this).attr('data-toggle', '0');
-	}
+  	if (window.matchMedia('(max-width: 767px)').matches) {
+        
+    } else {
+	  	if($(this).attr('data-toggle') == 0){
+	  		$(this).find('i').removeClass('fa-navicon');
+	  		$(this).find('i').addClass('fa-arrow-left');
+	  		$(this).find('i').css('color', '#E1E2E2');
+			$('.menu').animate({
+		    	width:'20%'
+		    }), $(this).animate({
+		    	left: '15%',
+		    	top: '1%'
+		    }), $('.menu-items').fadeToggle();
+		    $(this).attr('data-toggle', '1');
+		}
+		else{
+			$(this).find('i').addClass('fa-navicon');
+	  		$(this).find('i').removeClass('fa-arrow-left');
+	  		$(this).find('i').css('color', '#1D2228');
+	  		$('.menu-items').fadeToggle(), $('.menu').animate({
+		    	width:'0%'
+		    }), $(this).animate({
+		    	left: '0%',
+		    	top: '0%'
+		    });
+		    $(this).attr('data-toggle', '0');
+		}	
+    }
   });
 });
